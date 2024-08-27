cd4e30122213837ed2c812b3890fe2db179c7371
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-08-27 11:36:40 +0530

    3D components now appearing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,28 @@
 import { Canvas } from '@react-three/fiber';
 import React from 'react';
 import { OrbitControls, useTexture } from '@react-three/drei';
+import Cylinder from '@/components/hollowCylindrical';
 import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing';
 
 const page = () => {
   return (
-    <Canvas>
-      <OrbitControls />
-      <ambientLight />
-    </Canvas>
-  )
-}
+    <>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight />
+        <Cylinder />
+        <EffectComposer>
+          <Bloom
+            mipmapBlur
+            intensity={4} // Lower intensity slightly
+            luminanceThreshold={0} // Adjust threshold to reduce the number of pixels affected
+            luminanceSmoothing={1} // Smoothing closer to 1 reduces the effect area
+          />
+          <ToneMapping adaptive={true} resolution={2000} maxLuminance={16} />
+        </EffectComposer>
+      </Canvas>
+    </>
+  );
+};
 
 export default page
--- src/components/hollowCylindrical.tsx
+++ src/components/hollowCylindrical.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { useTexture } from '@react-three/drei';
+import * as THREE from 'three';
 
-const hollowCylindrical = () => {
-  return (
-    <mesh >
-    <cylinderGeometry />
-    <meshBasicMaterial />
-  </mesh>
-  )
-}
+const hollowCylindrical: React.FC = () => {
+    const tex = useTexture("./image.png");
+
+    const hollowCylindrical = useRef<THREE.Mesh>(null);
+
+    return (
+        <>
+            <mesh ref={hollowCylindrical}>
+                <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+                <meshBasicMaterial
+                    map={tex}
+                    transparent
+                    side={THREE.DoubleSide}
+                />
+            </mesh>
+        </>
+    );
+};
 
 export default hollowCylindrical
